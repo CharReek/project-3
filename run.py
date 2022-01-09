854f1bd3925422b001d4d77cc19373437bfc2558
CharReek <charlottereek@outlook.com>
2022-01-09 20:00:33 +0000

    delete code to start over

--- run.py
+++ run.py
@@ -7,4 +7,14 @@ def get_word():
     """
     selects a random word from the list of words
     """
-    return (random.choice(words)).lower()
+    return (random.choice(words)).upper()
+
+
+def play_game():
+    """
+    hangman game function
+    """
+    word = get_word()
+    word_letters 
+
+    lives = 10
