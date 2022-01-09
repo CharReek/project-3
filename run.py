e6655d25de51599f7a2962661c848ef54f0f46a1
CharReek <charlottereek@outlook.com>
2022-01-09 20:11:05 +0000

    add beggining function to letter guesses

--- run.py
+++ run.py
@@ -1,5 +1,17 @@
 word = "python"
 
-lives = 10 
-guesses = []
+lives = 10
+guesses = ["e"]
 completed = False
+
+# game play section
+
+while not completed:
+    for letter in word:
+        if letter in guesses:
+            print(letter, end=" ")
+        else:
+            print("_", end=" ")
+
+print("")
+completed = True
