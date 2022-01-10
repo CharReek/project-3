3706fe40778b033798b88eb9ba3da0eace8922cc
CharReek <charlottereek@outlook.com>
2022-01-10 19:04:06 +0000

    add start of game play function

--- run.py
+++ run.py
@@ -1,6 +1,16 @@
 import random
 from words import words
+ 
+# select word function
 
 def select_word():
     word = random.choice(words)
     return word.upper()
+
+# game play function 
+
+def game_play(word):
+    complete_word = "_" * len(word)
+    guess = False
+    words_guessed = []
+    letters_guesses = []
