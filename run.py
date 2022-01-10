e8927d01252cc4a5fa0d05788c27d9b53181d351
CharReek <charlottereek@outlook.com>
2022-01-10 19:22:57 +0000

    add in hangman visuals

--- run.py
+++ run.py
@@ -3,14 +3,93 @@ from words import words
  
 # select word function
 
+
 def select_word():
     word = random.choice(words)
     return word.upper()
 
 # game play function 
 
+
 def game_play(word):
     complete_word = "_" * len(word)
     guess = False
     words_guessed = []
     letters_guesses = []
+    lives = 6
+    print("Lets play a game of Hangman")
+    print(hangman_pic(tries))
+
+# hangman code taken from a youtube video 
+def hangman_pic(tries):
+    stages = [  # final state: head, torso, both arms, and both legs
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \|/
+                   |      |
+                   |     / \
+                   -
+                """,
+                # head, torso, both arms, and one leg
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \|/
+                   |      |
+                   |     / 
+                   -
+                """,
+                # head, torso, and both arms
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \|/
+                   |      |
+                   |      
+                   -
+                """,
+                # head, torso, and one arm
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \|
+                   |      |
+                   |     
+                   -
+                """,
+                # head and torso
+                """
+                   --------
+                   |      |
+                   |      O
+                   |      |
+                   |      |
+                   |     
+                   -
+                """,
+                # head
+                """
+                   --------
+                   |      |
+                   |      O
+                   |    
+                   |      
+                   |     
+                   -
+                """,
+                # initial empty state
+                """
+                   --------
+                   |      |
+                   |      
+                   |    
+                   |      
+                   |     
+                   -
+                """
+    ]
