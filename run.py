7ed312c9ab6f8f68868aae04a25373cfa58e5153
CharReek <charlottereek@outlook.com>
2022-01-10 18:42:25 +0000

    restart code

--- run.py
+++ run.py
@@ -1,17 +1,1 @@
 word = "python"
-
-lives = 10
-guesses = ["e"]
-completed = False
-
-# game play section
-
-while not completed:
-    for letter in word:
-        if letter in guesses:
-            print(letter, end=" ")
-        else:
-            print("_", end=" ")
-
-print("")
-completed = True
