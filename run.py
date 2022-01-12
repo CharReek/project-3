dc0501b9b0da3cdb0689be214ba88b0b0248e81f
CharReek <charlottereek@outlook.com>
2022-01-12 20:17:02 +0000

    add in hangman pic call

--- run.py
+++ run.py
@@ -16,23 +16,26 @@ def select_word():
 
 
 def game_play(word):
+    """
+    game play function
+    """
     complete_word = "_" * len(word)
     guess = False
     words_guessed = []
     letters_guessed = []
-    lives = 6
+    tries = 6
     print("Let's play a game of Hangman")
-    print(hangman_pic())
+    print(hangman_pic(tries))
     print(complete_word)
     print("\n")
-    while not guess and lives > 0:
+    while not guess and tries > 0:
         guessed = input("Please input a word or a letter: ").upper()
         if len(guessed) == 1 and guessed.isalpha():
             if guessed in letters_guessed:
                 print("You already gussed that letter, please try again")
             elif guessed not in word:
                 print(f"{guessed} is not in the word")
-                lives -= 1
+                tries -= 1
                 letters_guessed.append(guessed)
             else:
                 print("well done you guessed correctly!")
@@ -55,14 +58,14 @@ def game_play(word):
                 print("you have already guessed this word")
             elif guessed != word:
                 print("That guess is incorrect, Please try again")
-                lives -= 1
+                tries -= 1
                 words_guessed.append(guessed)
             else:
                 guessed = True
                 complete_word = word
         else:
             print("This is not a valid guess, please try again.")
-        print(hangman_pic())
+        print(hangman_pic(tries))
         print(complete_word)
         print("\n")
     if guess:
@@ -73,9 +76,9 @@ def game_play(word):
 # hangman code taken from a youtube video
 
 
-def hangman_pic():
+def hangman_pic(tries):
 
-    lives = [  # final state: head, torso, both arms, and both legs
+    stages = [  # final state: head, torso, both arms, and both legs
         """
                    --------
                    |      |
@@ -146,6 +149,7 @@ def hangman_pic():
                    -
                 """
     ]
+    return stages[tries]
 
 
 def start():
